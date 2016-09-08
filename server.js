e78801b2d490d2951c8590a400aaa22b0d0b3d30
2006110799 <chenxiang163com@163.com>
2016-09-08 23:18:21 +0800

    another way

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-var http = require("http");  
-http.createServer(function(request, response) {    
-    response.writeHead(200, {"Content-Type": "text/plain"});    
-    response.write("Hello World");    
-    response.end();  
-}).listen(80);  
-console.log("nodejs start listen 80 port!");
+var server = require("http");  
+server = http.createServer(function(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/html' //将返回类型由text/plain改为text/html
+    });
+    res.write('<h1>hello world!</h1>'); //返回HTML标签
+    res.end();
+});
