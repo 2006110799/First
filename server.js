8a9f2baed0e61944b77d84b1178c39d359296d9b
2006110799 <chenxiang163com@163.com>
2016-09-09 12:03:51 +0800

    Update and rename server.js to web.js

--- server.js
+++
@@ -1,8 +0,0 @@
-var http = require("http");  
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {
-        'Content-Type': 'text/html' //将返回类型由text/plain改为text/html
-    });
-    res.write('<h1>hello world!</h1>'); //返回HTML标签
-    res.end();
-}).listen(80);
